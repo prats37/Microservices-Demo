4ce738da723d02884499db3151aa6619d6c77652
Pratyush Raj <39978508+prats37@users.noreply.github.com>
2021-04-05 10:29:28 +0530

    Add files via upload

---
+++ MSI-ConfigServer/src/main/java/com/wipro/configserver/controller/ConfigController.java
@@ -0,0 +1,22 @@
+/*
+ * package com.wipro.configserver.controller;
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired; import
+ * org.springframework.boot.web.client.RestTemplateBuilder; import
+ * org.springframework.web.bind.annotation.GetMapping; import
+ * org.springframework.web.bind.annotation.RestController;
+ * 
+ * import com.netflix.discovery.EurekaClient;
+ * 
+ * @RestController public class ConfigController {
+ * 
+ * @Autowired EurekaClient eurekaClient;
+ * 
+ * @Autowired RestTemplateBuilder restTemplateBuilder;
+ * 
+ * @GetMapping("/") public String getWelcome() { return
+ * "### Message from Config Server ###"; }
+ * 
+ * 
+ * }
+ */
